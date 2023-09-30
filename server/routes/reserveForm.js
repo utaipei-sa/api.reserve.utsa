2f0b22f55db43f87ea014a69318e685903506360
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2023-09-30 15:10:57 +0800

    Refactor: Modify file structure
    Delete some unused files and rename some file

--- server/routes/reserveForm.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET reserve page. */
-router.get('/form', function(req, res, next) {
-    res.json({ title: 'Reserve Form' });
-});
-
-module.exports = router;
--- server/routes/signout.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET signout process. */
-router.get('/signout', function(req, res, next) {
-    res.json({ title: 'Sign out' });
-});
-
-module.exports = router;
