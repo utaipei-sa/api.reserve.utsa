0b2552f8d2251fcf6d9adc0e3ed6b7bb55cc1002
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2023-10-01 22:56:55 +0800

    Refactor: Remove /client
    Remove original React.js content (/client)

--- client/src/components/signin_email.js
+++
@@ -1,8 +0,0 @@
-export default function EmailBlock() {
-    return (
-        <div>
-            <label for="username">Username</label>
-            <input name="username" id="loginform.username" autocomplete="username webauthn" />
-        </div>
-    );
-}
--- client/src/pages/signin.js
+++
@@ -1,9 +0,0 @@
-import { template } from 'template.js';
-import { EmailBlock } from 'signin_email.js';
-
-export default function app() {
-    return template(
-        'Sign In or Sign Up', 
-        <EmailBlock />
-    );
-}
