b9f75b6d25f8ba969bc7119adacd8471833eecbd
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2023-10-05 10:38:05 +0800

    Refactor: Replace strings with a variable
    Replace path strings which are the same with a const.

--- server/routes/reserve/index.js
+++ server/routes/reserve/index.js
@@ -1,11 +1,13 @@
 const reserve_router = require('express').Router();
 
-reserve_router.use('/reserve', require('./reservation'));
-reserve_router.use('/reserve', require('./spaces'));
-reserve_router.use('/reserve', require('./items'));
-reserve_router.use('/reserve', require('./spaceAvailability'));
-reserve_router.use('/reserve', require('./spaceAvailabilities'));
-reserve_router.use('/reserve', require('./itemAvailability'));
-reserve_router.use('/reserve', require('./itemAvailabilities'));
+const root = '/reserve';
+
+reserve_router.use(root, require('./reservation'));
+reserve_router.use(root, require('./items'));
+reserve_router.use(root, require('./spaces'));
+reserve_router.use(root, require('./spaceAvailability'));
+reserve_router.use(root, require('./spaceAvailabilities'));
+reserve_router.use(root, require('./itemAvailability'));
+reserve_router.use(root, require('./itemAvailabilities'));
 
 module.exports = reserve_router;
