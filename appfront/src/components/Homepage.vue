b38e0bc1967066b13a817d4de9f07d3cce869a9b
terchang <terry.chang12@gmail.com>
2024-02-04 00:40:15 +0800

    Update Homepage.vue

--- appfront/src/components/Homepage.vue
+++ appfront/src/components/Homepage.vue
@@ -1,5 +1,52 @@
 <template>
-
+<v-sheet rounded="rounded" color="grey-lighten-4">
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-img
+          src=""
+          >
+          <!--pic for homepage-->
+        </v-img>
+        </v-card>
+      </v-col>
+    </v-row>   
+    <v-row justify="center">      
+      <v-col>
+        <v-card color="transparent" elevation="0">
+          <v-card-text>
+            <!--以下為提醒事項-->
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+  <v-container>
+    <v-row justify="center">
+      <v-col>
+        <v-card color="transparent" elevation="0">
+          <v-card-text>
+            <v-row>
+              <v-col class="v-col-auto">
+                <v-card title="聯絡我們" color="grey-lighten-1">
+                  
+                </v-card>
+              </v-col>
+            </v-row>
+            
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</v-sheet>
 </template>
 
 <script setup>
