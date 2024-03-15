020a819e2d3f956770a6f6037af99b5940861456
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2024-03-16 02:09:35 +0800

    Feat: Items and spaces list
    - Add open (spaces) and exception_time (spaces and items)
    - Modify API docs
    - Modify some syntax

--- backend/routes/reserve/GET_items.js
+++ backend/routes/reserve/GET_items.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var { items } = require('../../models/mongodb');
-var router = express.Router();
+const express = require('express')
+const { items } = require('../../models/mongodb')
+const router = express.Router()
 
 /**
  * @openapi
@@ -17,17 +17,22 @@ var router = express.Router();
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#/components/schemas/Item'
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   description: 物品清單
+ *                   items:
+ *                     $ref: '#/components/schemas/Item'
  */
 router.get('/items', async function(req, res, next) {
-    const data = await items.find({})
-                             .project({ _id: 1, name: 1, quantity: 1})
-                             .toArray(function(err, results){
-                                 console.log(results);
-                             });
-    res.json({data: data});
-    
+    const data = await items
+        .find({})
+        .project({ _id: 1, name: 1, quantity: 1, exception_time: 1 })
+        .toArray( function(err, results) {
+            console.log(results)
+        })
+    res.json({ data: data })
+})
 
-});
-
-module.exports = router;
+module.exports = router
