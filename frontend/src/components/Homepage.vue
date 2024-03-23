3cec0eae4aa24cdd819cf3ea5eea6c4d1da6a49b
terchang <terry.chang12@gmail.com>
2024-03-23 23:48:53 +0800

    Update Homepage.vue

--- frontend/src/components/Homepage.vue
+++ frontend/src/components/Homepage.vue
@@ -12,30 +12,30 @@
         </v-card>
       </v-col>
     </v-row>   
-    <v-row justify="center">      
+    <v-row class="d-flex justify-center">      
       <v-col>
-        <v-card color="transparent" elevation="0">
+        <v-card class="grey-lighten-4 text-center">
           <v-card-text>
             <!--以下為提醒事項-->
-            <ul class="message">
+            <div class="message">
                 <li v-for="item in RemindMessage" :key="item">{{ item }}</li>
-            </ul>
+            </div>
           </v-card-text>
         </v-card>
       </v-col>
     </v-row>
   </v-container>
   <v-container>
-    <v-row justify="center">
+    <v-row class="d-flex justify-center">
       <v-col>
-        <v-card color="transparent" elevation="0">
+        <v-card class="bg-grey-lighten-4">
           <v-card-text>
-              <v-row align="center">
+              <v-row class="d-flex justify-center">
                 <v-col>
                   <v-card title="聯絡我們" color="grey-lighten-1">
                     <v-card-text>
-                      <v-row align="center">
-                        <v-col>
+                      <v-row class="d-flex justify-center">
+                        <v-col >
                           <v-btn text href="https://www.instagram.com/utaipei_sa/">學生會IG</v-btn>
                         </v-col>
                         <v-col>
