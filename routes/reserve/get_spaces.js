3d4604ef71125dd1df0ad599710c468867fd2c7e
Chun-Cheng Yu <61136202+Chun-Cheng@users.noreply.github.com>
2024-06-28 22:28:45 +0800

    Refactor: Simplify some code
    Resolve:
    - https://github.com/utaipei-sa/api.reserve.utsa/pull/55#discussion_r1658694126
    - https://github.com/utaipei-sa/api.reserve.utsa/pull/55#discussion_r1658697429

--- routes/reserve/get_spaces.js
+++ routes/reserve/get_spaces.js
@@ -31,14 +31,12 @@ router.get('/spaces', async function (req, res, next) {
     .find({})
     .project({ _id: 1, name: 1, open: 1, exception_time: 1 })
     .toArray()
-  const return_data = data.map((element) => {
-    return {
-      _id: element._id,
-      name: element.name,
-      open: element.open,
-      exception_time: element.exception_time
-    }
-  })
+  const return_data = data.map((element) => ({
+    _id: element._id,
+    name: element.name,
+    open: element.open,
+    exception_time: element.exception_time
+  }))
   res.json({ data: return_data })
 })
 
