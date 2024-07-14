fd275250cc262ecdd3a6b45beaacaddbe587f13a
nickchengtw <33611456+nickchengtw@users.noreply.github.com>
2024-07-14 15:42:56 +0800

    Send email when making reservation

--- utilities/email/email.js
+++ utilities/email/email.js
@@ -1,5 +1,4 @@
 import nodemailer from 'nodemailer' // email relative
-// const nodemailer = require('nodemailer') // email relative
 
 /**
  * 寄送 email
@@ -7,30 +6,32 @@ import nodemailer from 'nodemailer' // email relative
  * @param {string} subject 主旨
  * @param {string} content 信件內容
  */
-export default function sendEmail(toEmail, subject, content) {
+export default async function sendEmail (toEmail, subject, content) {
   // 創建 SMTP 傳輸器
   const transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
       user: process.env.EMAIL,
-      pass: process.env.PASSWORD // 你的 Gmail 密碼或應用程式特定密碼 
+      pass: process.env.EMAIL_PASSWORD // 你的 Gmail 密碼或應用程式特定密碼
     }
   })
-  
+
   // 郵件內容 utsa@go.utaipei.edu.tw
   const mailOptions = {
     from: process.env.EMAIL, // 你的 Gmail 地址
     to: toEmail, // 使用者填寫的郵箱地址
-    subject : subject ,
+    subject,
     html: content // 郵件正文內容
   }
-  
+
   // 發送郵件
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error)
-    } else {
-      console.log('The email has sent: ' + info.response)
-    }
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        reject(error) // Reject the promise with the error
+      } else {
+        resolve(info.response) // Resolve the promise with the response info
+      }
+    })
   })
 }
--- utilities/response.js
+++ utilities/response.js
@@ -2,6 +2,7 @@ export const R_SUCCESS = 'R_SUCCESS'
 export const R_INVALID_INFO = 'R_INVALID_INFO'
 export const R_ID_NOT_FOUND = 'R_ID_NOT_FOUND'
 export const R_INVALID_RESERVATION = 'R_INVALID_RESERVATION'
+export const R_SEND_EMAIL_FAILED = 'R_SEND_EMAIL_FAILED'
 
 export function error_response (error_code, message) {
   return { error_code, message }
