ffba17eda240e4909a25a734e776c2c423715d1f
barney9211 <barney9211@gmail.com>
2024-08-05 12:09:38 +0800

    Fix:發現不小心把120行的_id的objectId刪掉了

--- routes/reserve/delete_reserve.js
+++ routes/reserve/delete_reserve.js
@@ -117,7 +117,7 @@ router.delete('/reserve/:reservation_id', async function (req, res, next) {
       })
     }
 
-    const reservation_result = await reservations.deleteMany({ _id: reservation_id })
+    const reservation_result = await reservations.deleteMany({ _id: new ObjectId(reservation_id) })
     await items_reserved_time.deleteMany({ reserved_quantity: 0 })
     await spaces_reserved_time.deleteMany({ reserved: 0 })
     if (reservation_result.deletedCount > 0) {
