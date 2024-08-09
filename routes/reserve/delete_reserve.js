0d54a02369650c763e78947f8c618d1f405d9f41
barney9211 <barney9211@gmail.com>
2024-08-09 15:21:33 +0800

    Fix:conflict

--- routes/reserve/delete_reserve.js
+++ routes/reserve/delete_reserve.js
@@ -84,50 +84,59 @@ router.delete('/reserve/:reservation_id', async function (req, res, next) {
 
     const item_reserved_time_find = await items_reserved_time.find({ reservation_id: { $in: [new ObjectId(reservation_id)] } }).toArray()
     const space_reserved_time_find = await spaces_reserved_time.find({ reservation_id: { $in: [new ObjectId(reservation_id)] } }).toArray()
-
+    console.log('\x1B[36m%s\x1B[0m', "console.log--------------------------------------------------------------------------")
+    console.log('\x1B[36m%s\x1B[0m', "reservation_id:           " + req.params.reservation_id)
+    console.log('\x1B[36m%s\x1B[0m', "item_find_array_length:   " + item_reserved_time_find.length)
+    console.log('\x1B[36m%s\x1B[0m', "space_find_array_length:  " + space_reserved_time_find.length)
     // delete items_reserved_time
 
     for (const item of item_reserved_time_find) {
       let quantity = 0
       const index = storeReserveInfo.findIndex(e => e.item_id === item.item_id)
       quantity = item.reserved_quantity - storeReserveInfo[index].quantity
-
+      
+      console.log('\x1B[36m%s\x1B[0m', "update item_id:           "+ item_id)
+      console.log('\x1B[36m%s\x1B[0m', "after delete quantity:    " + quantity)
       await items_reserved_time.updateOne({
         _id: item._id// filter
       },
-      {
-        $set: {
-          reserved_quantity: quantity// change data
-        },
-        $pull: {
-          reservation_id: new ObjectId(reservation_id)
-        }
-      })
+        {
+          $set: {
+            reserved_quantity: quantity// change data
+          },
+          $pull: {
+            reservation_id: new ObjectId(reservation_id)
+          }
+        })
     }
 
     // space
     for (const space of space_reserved_time_find) {
+      console.log('\x1B[36m%s\x1B[0m', "update space_id:           "+ space_id)
       await spaces_reserved_time.updateOne({
         _id: space._id// filter
       },
-      {
-        $set: {
-          reserved: 0// change data
-        },
-        $pull: {
-          reservations: new ObjectId(reservation_id)
-        }
-      })
+        {
+          $set: {
+            reserved: 0// change data
+          },
+          $pull: {
+            reservations: new ObjectId(reservation_id)
+          }
+        })
     }
 
     const reservation_result = await reservations.deleteMany({ _id: new ObjectId(reservation_id) })
     await items_reserved_time.deleteMany({ reserved_quantity: 0 })
     await spaces_reserved_time.deleteMany({ reserved: 0 })
+    console.log('\x1B[36m%s\x1B[0m', "reservation_result.deletedCount:     "+ reservation_result.deletedCount)
     if (reservation_result.deletedCount > 0) {
       res
         .status(200)
         .json({ code: R_SUCCESS, message: 'Success!' })
     }
+    console.log('\x1B[36m%s\x1B[0m',"console.log end--------------------------------------------------------------------------")
+
   }
 })
 
