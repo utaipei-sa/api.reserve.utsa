b3a6ed7a8cddf225c2764929f9fc84b53d0ae020
JEFF Liao <jeffliao92316046@gmail.com>
2024-09-20 22:08:38 +0800

    refactor :reserve repository

--- repositories/reserve_repository.js
+++ repositories/reserve_repository.js
@@ -1,5 +1,36 @@
+import { ObjectId } from 'mongodb'
+import { reservations } from '../models/mongodb.js'
 class ReserveRepository {
+  getReserveById = async (/** @type {string} */ id) => {
+    return await reservations.findOne({
+      _id: { $eq: new ObjectId(id) }
+    })
+  }
 
+  deleteReserveById = async (/** @type {string} */ id) => {
+    return await reservations.deleteMany({ _id: new ObjectId(id) })
+  }
+
+  insertReserve = async (/** @type {Object} */ doc) => {
+    await reservations.insertOne(doc)
+  }
+
+  updateReserveById = async (
+    /** @type {string} */ id,
+    /** @type {object} */ reservation
+  ) => {
+    await reservations.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: reservation }
+    )
+  }
+
+  updateVerifyById = async (/** @type {string} */ id) => {
+    await reservations.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: { verify: 1 } }
+    )
+  }
 }
 
 export default new ReserveRepository()
